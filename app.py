3d90ceae58c265d3ceecb6987ea99a158587faec
richkats <katsur.serg@gmail.com>
2021-04-18 22:15:49 +0500

    Have to work (probably)

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template
 import requests as req
 
 app = Flask(__name__)
 
-attribs = ["style=text-decoration:none;" for i in range(3)]
-words = set([f'nothing {i+1}' for i in range(3)])
+# attribs = ["style=text-decoration:none;" for i in range(3)]
+# words = set([f'nothing {i+1}' for i in range(3)])
+words = []
 
 
 def rand_word():
@@ -14,24 +15,27 @@ def rand_word():
     return r.json()["word"]["word"]
 
 
-@app.route('/<new>')
-@app.route('/index/<new>')
-def index(new=None):
-    if new == 'new':
-        global attribs
-        attribs = ["style=text-decoration:none;" for i in range(3)]
-        global words
-        while True:
-            words = set([rand_word() for i in range(3)])
-            if len(words) == 3:
-                break
-    if new in ['0', '1', '2']:
-        attribs[int(new)] = "style=display:none;"
-        '''
-        if attribs.count("style=display:none;") == 3:
-            return redirect("/index/new")
-        '''
-    return render_template('index.html', words=list(words), attribs=attribs)
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/set_words/<ws>')
+def set_words(ws):
+    global words
+    words = ws[1:-1].replace('\'', '').split(', ')
+    return ''.join(words)
+
+
+@app.route('/get_words/<id>')
+def get_words(id):
+    id = int(id)
+    global words
+    if id <= len(words)-1:
+        return words[id]
+    else:
+        return ""
 
 
 if __name__ == "__main__":
---
+++ parser.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+import time
+from selenium import webdriver
+import word_db as db
+import requests as req
+
+
+def guesser(sel_driver):
+    words = db.get_words()
+    req.get(f'http://127.0.0.1:5000/set_words/{[word.russian for word in words]}')
+    while True:
+        time.sleep(1)
+        try:
+            # guessings = sel_driver.find_elements_by_class_name("chat-line__message")
+            # guessings = sel_driver.find_elements_by_xpath("//*[@data-a-target='chat-message-text']")
+            nicknames = sel_driver.find_elements_by_xpath("//*[@data-a-target='chat-message-username']")
+            messages = sel_driver.find_elements_by_xpath("//*[@data-a-target='chat-message-text']")
+            for i in range(len(messages)):
+                word_guessed = db.compare(messages[i].text.lower(), words)
+                if word_guessed:
+                    print(f"{nicknames[i].text} guessed word {word_guessed.russian} ({word_guessed.english})")
+                    req.get(f'http://127.0.0.1:5000/set_words/{[word.russian for word in words]}')
+
+        except Exception:
+            continue
+
+
+def login(username, password, driver):
+    driver.get('https://www.twitch.tv/moderator/zageev')
+    search_form = driver.find_element_by_id('login-username')
+    search_form.send_keys(username)
+    search_form = driver.find_element_by_id('password-input')
+    search_form.send_keys(password)
+    search_form.submit()
+    driver.implicitly_wait(10)
+
+
+def start_driver():
+    global chrome_options, driver
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--mute-audio")
+
+    driver = webdriver.Chrome(chrome_options=chrome_options)  # Optional argument, if not specified will search path.
+    driver.get('https://www.twitch.tv/zageev')
+
+
+start_driver()
+while True:
+    time.sleep(1)
+    try:
+        events = driver.find_elements_by_class_name('tw-c-text-alt-2')
+        for event in events:
+            if "Guess 3 russian words" in event.text:
+                print(event.text)
+                guesser(driver)
+                driver.quit()
+                start_driver()
+                break
+    except Exception:
+        continue
+
+
+
+
+#login(USER, PASS)
+
+#time.sleep(5) # Let the user actually see something!
+#search_box = driver.find_element_by_name('q')
+#text = input("INSERT YOUR TEXT: ")
+#search_box.send_keys(text)
+#search_box.submit()
+time.sleep(60) # Let the user actually see something!
+'''
+for i in range(5):
+    sites = driver.find_elements_by_tag_name("link")
+    for site in sites:
+        print(site.get_attribute("href"))
+    time.sleep(5)
+'''
+
+driver.quit()
